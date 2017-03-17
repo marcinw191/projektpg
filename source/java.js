f230c9fb396b05e7f7f62d870c356dfb6e582ccb
Artur Leśny <toudik99@gmail.com>
2017-03-17 18:32:08 +0100

    Przeniesienie parametru ilości zleceń na stronie jako "parametr" funkcji; wydzielenie z funkcji podfunkcji odpowiedzialnej za generowanie wyglądu pojedyńczego zgłoszenia.

--- source/java.js
+++ source/java.js
@@ -1,12 +1,20 @@
-function Body(){
-    var k = 10;  // parametr określający ilość wyświetlanych zleceń na stronie
+function Body(il_zlecen){
+    // il_zlecen - parametr określający ilość wyświetlanych zleceń na stronie
     var ogloszenie = ""; //zawartość html dla części <div id="main">
     // pętla wykonywana określoną przez parametr "k" ilość razy;
     // przy okazji każde zlecenie ma swoje id na stronie
-    for (var i=0 ; i<k ; i++) {
+    for (var i=0 ; i< il_zlecen ; i++) {
         var etykieta_id="zlecenie_"+i;
         console.log(etykieta_id);
-        ogloszenie=ogloszenie+'<img id="'+etykieta_id+'" src="../img/zlecenie.jpg">';
+        ogloszenie=ogloszenie+Zlecenie(etykieta_id);
         }
     document.getElementById('main').innerHTML = ogloszenie;
 }
+
+function Zlecenie(etykieta){
+    // poniższy wiersz do późniejszej modyfikacji
+    // odpowiada za generowanie wyglądu "zlecenia"
+    var tresc='<img id="'+etykieta+'" src="../img/zlecenie.jpg">';
+    return tresc;
+}
+
