21e3b221b9a5e959a9f7118ae400bb0e0182eeda
Bart <bartlomiej.kornowski@gmail.com>
2017-03-31 19:15:40 +0200

    Dodanie routingu do aplikacji, wydzielenie glowengo widoku do nowego komponentu galeria-ogloszen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { OgloszenieMiniaturaComponent } from './ogloszenie-miniatura/ogloszenie-miniatura.component';
 import { OgloszenieComponent } from './ogloszenie/ogloszenie.component';
+import { GaleriaOgloszenComponent } from './galeria-ogloszen/galeria-ogloszen.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +18,29 @@ import { OgloszenieComponent } from './ogloszenie/ogloszenie.component';
     NavbarComponent,
     FooterComponent,
     OgloszenieMiniaturaComponent,
-    OgloszenieComponent
+    OgloszenieComponent,
+    GaleriaOgloszenComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot([
+      {
+        path: 'ogloszenie',
+        component: OgloszenieComponent
+      },
+      {
+        path: 'ogloszenia',
+        component: GaleriaOgloszenComponent
+      },
+      {
+        path: '',
+        redirectTo: '/ogloszenia',
+        pathMatch: 'full'
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/galeria-ogloszen/galeria-ogloszen.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GaleriaOgloszenComponent } from './galeria-ogloszen.component';
+
+describe('GaleriaOgloszenComponent', () => {
+  let component: GaleriaOgloszenComponent;
+  let fixture: ComponentFixture<GaleriaOgloszenComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GaleriaOgloszenComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GaleriaOgloszenComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
