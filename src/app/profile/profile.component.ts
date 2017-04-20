b1d6104bd5d1a538d9d817c2e3bacc0d53ba5284
Artur Leśny <toudik99@gmail.com>
2017-04-20 22:03:08 +0200

    Dotyczy zadania #37 :
    plik profiles.ts - tymczasowa "baza" z przykładowymi danymi;
    poprawiono szablon tak aby wyświetlał dane z "jednej" zmiennej, a nie z różnych źródeł;
    dane z "bazy" mają wyższy priorytet przy wyświetlaniu niż dostarczane przez serwer autoryzacyjny;

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input }   from '@angular/core';
+import { Component, OnInit }   from '@angular/core';
+
+import { Profil }      from './profile';
+import { PROFILES }    from './profiles';
+
 import { AuthService } from '../auth0/auth.service';
 
 import * as moment from 'moment-timezone';
@@ -10,15 +14,10 @@ import * as moment from 'moment-timezone';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  selectedProfil: Profil;
   profile:any;
   time:any;
-
-  funkcja: string = 'zleceniodawca';
-
-  zmienFunkcja(funkcja: string) {
-    this.funkcja = funkcja;
-  }
+  id:string ='0';
 
   constructor(private auth: AuthService) {
     this.profile = JSON.parse(localStorage.getItem('profile'));
@@ -27,9 +26,42 @@ export class ProfileComponent implements OnInit {
     console.log(this.time);
   }
 
-  ngOnInit(){
-    // this.profil.zdjecie = this.profile.picture;
-    // this.profil.nazwa   = this.profile.name;
-    // this.profil.email   = this.profile.email;
+  changeRole(funkcja: string) {
+    this.selectedProfil.funkcja = funkcja;
+  }
+
+  ngOnInit(): void {
+    // porównanie adresu e-mail z autoryzacji z adresami e-mail z "bazy"
+    // jeśli adresy są identyczne, to pobierany jest id rekordu w bazie
+    for (var x=0; x<PROFILES.length; x++) {
+      if (PROFILES[x].email ==  this.profile.email) {
+        this.id=PROFILES[x].id;
+      }
+    }
+    // przekopiowanie danych z bazy do zmiennej, której wartości są wyświetlane na stronie
+    this.selectedProfil         = PROFILES[parseInt(this.id)];
+    // na razie na stałe przepisanie danych z autoryzacji do zmiennej, której wartości są wyświetlane na stronie
+    if (this.selectedProfil.zdjecie=='') {
+      this.selectedProfil.zdjecie = this.profile.picture;}
+    if (this.selectedProfil.nazwa=='') {
+      this.selectedProfil.nazwa   = this.profile.name;}
+    if (this.selectedProfil.email=='') {
+      this.selectedProfil.email   = this.profile.email;}
+  }
+
+  saveProfile(){
+    alert('Profil zapisany');
+  }
+
+  cancelProfile(){
+    alert('Zrezygnowano ze zmian w profilu');
+  }
+
+  listOfOrders(){
+    alert('Przygotowywanie zestawienia zleceń');
+  }
+
+  listOfOfferts(){
+    alert('Przygotowywanie zestawienia ofert');
   }
 }
