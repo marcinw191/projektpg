1dd7b22e38be193ed98b712a2834f97cf582c132
Artur Leśny <toudik99@gmail.com>
2017-05-01 22:52:24 +0200

    Pooprawienie komponentu "czasu logowania" o mechanizm weryfikacji czy użytkownik jest zalogowany, jeśli nie, to komponent nie jest wyświetlany.

--- src/app/login-time/login-time.component.ts
+++ src/app/login-time/login-time.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService }             from '../serwisy/auth0/auth.service';
+
+import { AuthService }       from '../serwisy/auth0/auth.service';
 
 import * as moment from 'moment-timezone';
 
@@ -15,10 +16,12 @@ export class LoginTimeComponent implements OnInit {
   constructor(private auth: AuthService) { }
 
   ngOnInit() {
-    this.profile = JSON.parse(localStorage.getItem('profile'));
-    console.log(this.profile);
-    this.time = moment(this.profile.updated_at).tz("Europe/Warsaw").format('YYYY-MM-DD HH:mm:ss');
-    console.log(this.time);
+    if (this.auth.authenticated()) {
+      this.profile = JSON.parse(localStorage.getItem('profile'));
+      console.log(this.profile);
+      this.time = moment(this.profile.updated_at).tz("Europe/Warsaw").format('YYYY-MM-DD HH:mm:ss');
+      console.log(this.time);
+    }
   }
 
 }
