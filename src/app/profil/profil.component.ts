3c4538f3cb6723b3fd1d7b15bbba6fb0a09e4978
Artur Leśny <toudik99@gmail.com>
2017-05-01 22:50:53 +0200

    Dotyczy zadania #55 : poprawienie komponentu o możliwość edycji profilu.

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router}             from '@angular/router';
 
 import { AuthService }             from '../serwisy/auth0/auth.service';
 import { BazaUzytkownikowService } from '../serwisy/bazauzytkownikow.service';
-
-import { Profil }                  from './profil';
 
 @Component({
   selector: 'app-profil',
@@ -13,7 +12,7 @@ import { Profil }                  from './profil';
 
 export class ProfilComponent implements OnInit {
   users : any[];
-  profil:Profil = {
+  profil: any = {
     zdjecie     : '',
     nazwa       : '',
     e_mail      : '',
@@ -28,36 +27,74 @@ export class ProfilComponent implements OnInit {
   edycja:boolean;
   wybor:number = null;
 
-  constructor(private auth: AuthService, private bazaUzytkownikowService:BazaUzytkownikowService){
+  constructor(private auth: AuthService,
+              private bazaUzytkownikowService:BazaUzytkownikowService,
+              private router: Router){
   }
 
   ngOnInit() {
-    this.edycja=false;
-    // profil pobrany z cookie procesu auth0
-    this.profil_auth = JSON.parse(localStorage.getItem('profile'));
-    // pobranie bazy użytkowników i sprawdzenie, czy jest już taku użytkownik
-    this.bazaUzytkownikowService.getUsers().subscribe(users =>
-      {
-      this.users = users;
-        // porównanie adresu e-mail z autoryzacji z adresami e-mail z "bazy"
-        for (var x=0; x<this.users.length; x++) {
-          if (this.users[x].e_mail ==  this.profil_auth.email) {
-            this.wybor=x;
+    if (this.auth.authenticated()) {
+      this.edycja=false;
+      // profil pobrany z pliku cookie procesu auth0
+      this.profil_auth = this.auth.getProfileAuth();
+      console.log(this.profil_auth);
+      // pobranie profilu z bazy użytkowników na podstawie adresu e-mail
+      this.bazaUzytkownikowService.getUsers().subscribe(users =>
+        {
+        this.users = users;
+          // porównanie adresu e-mail z autoryzacji z adresami e-mail z "bazy"
+          for (let x=0; x<this.users.length; x++) {
+            if (this.users[x].e_mail ==  this.profil_auth.email) {
+              this.wybor=x;
+            }
+          }
+          console.log(this.wybor);
+          // jeśli adresu nie ma w bazie, to profil jest tworzony w bazie
+          if (this.wybor == null)  {
+            this.profil.zdjecie = this.profil_auth.picture;
+            this.profil.nazwa = this.profil_auth.name;
+            this.profil.e_mail = this.profil_auth.email;
+            this.profil.typ = 'zleceniodawca';
+            this.bazaUzytkownikowService.addUser(this.profil);
+            console.log(this.profil);
+          }
+          // jeśli adresy są identyczne, to pobierany jest profil z bazy
+          else {
+            this.profil = this.users[this.wybor];
+            console.log(this.profil);
           }
         }
-        // jeśli adresy są identyczne, to pobierany jest profil z bazy
-        if (this.wybor) {
-          this.profil = this.users[this.wybor];
-        }
-        // jeśli adresu nie ma w bazie, to profil jest tworzony w bazie
-        else {
-          this.profil.zdjecie = this.profil_auth.picture;
-          this.profil.nazwa = this.profil_auth.name;
-          this.profil.e_mail = this.profil_auth.email;
-          this.profil.typ = 'zleceniodawca';
-          this.bazaUzytkownikowService.addUser(this.profil);
-        }
-      }
-    );
+      );
+    }
+    else {
+      alert('Nie jesteś zalogowany !!!');
+      this.router.navigateByUrl('/');
+    }
   }
+
+  edytujProfil(){
+    this.edycja=true;
+  }
+
+  powrotProfil(){
+    this.edycja=false;
+  }
+
+  listaOgloszen(){
+    alert('Przygotowywanie zestawienia zleceń');
+  }
+
+  listaOfert(){
+    alert('Przygotowywanie zestawienia ofert');
+  }
+
+  Administrator(){
+    this.router.navigateByUrl('/administrator');
+  }
+
+  zapiszProfil(){
+    this.bazaUzytkownikowService.updateUser(this.profil.$key,this.profil);
+    alert('Profil zapisany');
+  }
+
 }
