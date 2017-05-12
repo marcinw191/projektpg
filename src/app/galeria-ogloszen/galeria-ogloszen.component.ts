29e02e3dcfbcc6dfc60aff73a36dbbcd21a1ebfb
Bart <bartlomiej.kornowski@gmail.com>
2017-05-12 23:00:22 +0200

    #53 dodanie wyszukiwarki ogloszen

--- src/app/galeria-ogloszen/galeria-ogloszen.component.ts
+++ src/app/galeria-ogloszen/galeria-ogloszen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit }   from '@angular/core';
+import { Component, OnInit, OnChanges }   from '@angular/core';
 import { AuthService }         from '../serwisy/auth0/auth.service';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
@@ -9,15 +9,40 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 export class GaleriaOgloszenComponent implements OnInit {
 
+  fraza: string;
   orientacja: string = 'galeria';
-  miniatury: FirebaseListObservable<any>;
+  miniatury: Array<any>;
+  miniaturyBezFiltrowania: Array<any>;
+  pofiltrowane: boolean;
 
-  zmienOrientacje(orientacja: string) {
-      this.orientacja = orientacja;
-  }
 
   constructor(private auth: AuthService, private af: AngularFire) {
-      this.miniatury = af.database.list('/ogloszenia');
+    this.af.database.list('/ogloszenia').subscribe(queriedItems => {
+      this.miniatury = queriedItems;
+      this.miniaturyBezFiltrowania = queriedItems;
+    });
+    this.pofiltrowane = false;
+  }
+
+  public zmienOrientacje(orientacja: string) {
+    this.orientacja = orientacja;
+  }
+
+  public szukaj() {
+    this.pofiltrowane = true;
+    this.miniatury = this.miniaturyBezFiltrowania.filter(ogloszenie => {
+      let tytul = ogloszenie.tytul.toLowerCase();
+      if (tytul.indexOf(this.fraza.toLowerCase())!= -1)
+        return true;
+      else
+        return false;
+    });
+  }
+
+  public wyczyscSzukanie(){
+    this.miniatury = this.miniaturyBezFiltrowania;
+    this.pofiltrowane = false;
+    this.fraza = "";
   }
 
   ngOnInit() {
