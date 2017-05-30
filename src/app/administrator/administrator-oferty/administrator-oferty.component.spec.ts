432c4f1b6725a55c0fa8a8070dc441be785e756b
Bart <bartlomiej.kornowski@gmail.com>
2017-05-30 17:18:41 +0200

    probna wersja mocka

--- src/app/administrator/administrator-oferty/administrator-oferty.component.spec.ts
+++ src/app/administrator/administrator-oferty/administrator-oferty.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { AdministratorOfertyComponent } from './administrator-oferty.component';
 import {AdministratorOfertaComponent} from "../administrator-oferta/administrator-oferta.component";
 import {ProfilUzytkownikComponent} from "../../profil/profil-uzytkownik/profil-uzytkownik.component";
 import {FormsModule} from "@angular/forms";
 import {BazaOfertService} from "../../serwisy/firebase-oferty/bazaofert.service";
+
+import { mockAngularFireDatabase } from '../../mocks/mock-angularfire';
+import {Observable} from "rxjs";
 
 let firebaseConfig = {
   apiKey: "AIzaSyDIUpjNc8RE0NDMFmuW3LRYhuZwiH7R-Vo",
@@ -15,6 +18,25 @@ let firebaseConfig = {
   messagingSenderId: "846477355550"
 };
 
+let mockFirebase = {
+
+  list: function(table, params) {
+    if(table == '/oferty')
+    {
+      return Observable.of([
+        {
+          cena: 400,
+          dataPublikacji: "2017-05-11T18:34:01.485Z",
+          numerOgloszenia: "2",
+          oferent: "google-oauth2|114567725685047947770",
+          telefon: "502876123",
+          wiadomosc: "Chętnie obejrzę kuchnię i dogadamy się co do zakresu robót. Proszę o kontakt - mam wolne terminy."
+        }
+      ])
+    }
+  }
+
+};
 
 describe('AdministratorOfertyComponent', () => {
   let component: AdministratorOfertyComponent;
@@ -23,8 +45,11 @@ describe('AdministratorOfertyComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AdministratorOfertyComponent, AdministratorOfertaComponent, ProfilUzytkownikComponent ],
-      imports: [ FormsModule,  AngularFireModule.initializeApp(firebaseConfig) ],
-      providers: [ BazaOfertService, AngularFireModule ]
+      imports: [ FormsModule ],
+      providers: [
+        BazaOfertService,
+        { provide: AngularFireDatabase, useValue: mockFirebase}
+        ]
     })
     .compileComponents();
   }));
---
+++ src/app/mocks/mock-angularfire.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by bkornows on 28/05/2017.
+ */
+export class mockAngularFireDatabase {
+  public app: any;
+
+  constructor(){
+    this.app = {};
+
+  }
+
+  public list(table, params) {
+
+  }
+
+}
