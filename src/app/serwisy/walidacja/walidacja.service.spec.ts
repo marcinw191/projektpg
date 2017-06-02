a3caf56725b7859f6fc74b625e4ebe02615d82ea
Artur Leśny <toudik99@gmail.com>
2017-06-02 08:05:27 +0200

    Wprowadzono przykładowe testy jednostkowe dla serwisu "Walidacja".

--- src/app/serwisy/walidacja/walidacja.service.spec.ts
+++ src/app/serwisy/walidacja/walidacja.service.spec.ts
@@ -9,7 +9,20 @@ describe('WalidacjaService', () => {
     });
   });
 
-  it('should ...', inject([WalidacjaService], (service: WalidacjaService) => {
+  it('should create', inject([WalidacjaService], (service: WalidacjaService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('powinien sprawdzić poprawność formatu : kod pocztowego', inject([WalidacjaService], (service: WalidacjaService) => {
+    expect(service.walidacja('kod', '80-354')).toBe(true);
+    expect(service.walidacja('kod', '123456')).toBe(false);
+    expect(service.walidacja('kod', 'bc-def')).toBe(false);
+  }));
+
+  it('powinien sprawdzić poprawność formatu : adres e-mail', inject([WalidacjaService], (service: WalidacjaService) => {
+    expect(service.walidacja('email', 'ala@ala.pl')).toBe(true);
+    expect(service.walidacja('email', 'ala@pl.')).toBe(false);
+    expect(service.walidacja('email', 'ala.ala.pl')).toBe(false);
+  }));
+
 });
