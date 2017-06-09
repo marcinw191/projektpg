5fc5cd5823950cb3010649e23d23cec81a187012
Artur Leśny <toudik99@gmail.com>
2017-06-09 21:44:26 +0200

    Stworzenie pliku, w którym mają być przechowywane wartości, które wykorzystywane są przez różne komponenty i serwisy.

---
+++ src/app/app-variables.ts
@@ -0,0 +1,13 @@
+export const zleceniodawca: string = 'zleceniodawca';
+export const wykonawca: string = 'wykonawca';
+export const administrator: string = 'administrator';
+
+export const options: any = {
+  icon: '',
+  size: 'sm',
+  showCloseButton: false,
+  showCancelButton: true,
+  cancelButtonText: 'Cancel',
+  showConfirmButton: true,
+  confirmButtonText: 'OK'
+};
