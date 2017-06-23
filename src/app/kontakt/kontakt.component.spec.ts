84f50dd750e710c7e4a902eb6a4d594f7bc6d934
Artur Leśny <toudik99@gmail.com>
2017-06-23 22:55:34 +0200

    Dodano mechanizmy testów jednostkowych dla komponentu kontakt oraz serwisu sendmail.

--- src/app/kontakt/kontakt.component.spec.ts
+++ src/app/kontakt/kontakt.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MailsendService } from '../serwisy/mail/mailsend.service';
+import { WalidacjaService } from '../serwisy/walidacja/walidacja.service';
+import { GoogleMapsComponent } from '../googlemaps/googlemaps.component';
 import { KontaktComponent } from './kontakt.component';
+import { MockMail } from '../mocks/mock-mail';
+import { FormsModule } from '@angular/forms';
+import { MapsAPILoader } from 'angular2-google-maps/core';
+import { DialogService } from 'ngx-bootstrap-modal';
+
+class mapsAPILoader{
+  public load() {
+    return new Promise((resolve, reject) => { });
+  }
+}
 
 describe('KontaktComponent', () => {
   let component: KontaktComponent;
   let fixture: ComponentFixture<KontaktComponent>;
+  let mockMail = new MockMail();
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ KontaktComponent ]
+      imports: [ FormsModule ],
+      declarations: [
+        KontaktComponent,
+        GoogleMapsComponent
+      ],
+      providers: [
+        WalidacjaService,
+        DialogService,
+        { provide: MailsendService, useValue: mockMail.getMock() },
+        { provide: MapsAPILoader, useClass: mapsAPILoader },
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
